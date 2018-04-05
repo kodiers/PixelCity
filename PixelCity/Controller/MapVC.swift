26666267f359cd468680c9fd4329fb3b903a93ca
Victor Yamchinov <kodiers@gmail.com>
2018-04-05 03:36:00 +0300

    created project

--- PixelCity/Controller/MapVC.swift
+++ PixelCity/Controller/MapVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapVC.swift
 //  PixelCity
 //
 //  Created by Viktor Yamchinov on 05/04/2018.
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func centerMapBtnPressed(_ sender: Any) {
+    }
+    
+}
 
+extension MapVC: MKMapViewDelegate {
+    
 }
 
